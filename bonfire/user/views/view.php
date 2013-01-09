b2da103e55f39dd195a5be13d82037dfb79c0e1d
Yue <lichwy1024@gmail.com>
2013-01-08 20:18:16 -0800

    first commit

---
+++ bonfire/user/views/view.php
@@ -0,0 +1,14 @@
+<h2>new view</h2> 
+<h3 id='id'><?= $id ?></h3>
+
+<div id='video_part'>
+	<?php  echo $video_panel; ?>
+</div>
+
+<div id='review_part'>
+    <?php  echo $review_panel; ?>
+</div>
+
+<div id='comment_part' class = ''>
+	<?php  echo $comment_panel ?>
+</div>
