fb45b4c269d836835e38a6bd2e1130e0bfb66aca
Lixin Qin <cliffyq@gmail.com>
2013-02-04 21:35:10 -0500

    company

--- bonfire/modules/users/views/users/register.php
+++ bonfire/modules/users/views/users/register.php
@@ -14,72 +14,71 @@
 	</div>
 	<?php endif; ?>
 	
-	<div class="row-fluid">
+	<!-- 	<div class="row-fluid">
 		<div class="span8 offset2">
-			<div class="alert alert-info fade in">
-				<a data-dismiss="alert" class="close">&times;</a>
-				<h4 class="alert-heading"><?php echo lang('bf_required_note'); ?></h4>
-				<?php if (isset($password_hints) ) echo $password_hints; ?>
-			</div>
+		<div class="alert alert-info fade in">
+		<a data-dismiss="alert" class="close">&times;</a>
+		<h4 class="alert-heading"><?php// echo lang('bf_required_note'); ?></h4>
+		<?php //if (isset($password_hints) ) echo $password_hints; ?>
 		</div>
-	</div>
+		</div>
+	</div> -->
 	
 	<div class="row-fluid">
 		<div class="span12">
 			
 			<?php echo form_open('register', array('class' => "form-horizontal", 'autocomplete' => 'off')); ?>
-			
-			
-			<div class="control-group <?php echo iif( form_error('email') , 'error'); ?>">
+			<div class="control-group offset1 <?php echo iif( form_error('email') , 'error'); ?>">
 				<label class="control-label required" for="email"><?php echo lang('bf_email'); ?></label>
 				<div class="controls">
-					<input class="span4" type="text" name="email" id="email"  value="<?php echo set_value('email'); ?>"  placeholder="<?php echo lang('bf_email'); ?>" required="required" />
+					<input class="input-xlarge" type="text" name="email" id="email"  value="<?php echo set_value('email'); ?>"  required="required" />
 				</div>
 			</div>
+			
 			<?php if ( $this->settings_lib->item('auth.login_type') !== 'email' OR $this->settings_lib->item('auth.use_usernames') == 1): ?>
 			
-			<div class="control-group <?php echo iif( form_error('username') , 'error'); ?>">
+			<div class="control-group offset1 <?php echo iif( form_error('username') , 'error'); ?>">
 				<label class="control-label required" for="username"><?php echo lang('bf_username'); ?></label>
 				<div class="controls">
-					<input class="span4" type="text" name="username" id="username" value="<?php echo set_value('username') ?>" placeholder="<?php echo lang('bf_username'); ?>" required="required" />
+					<input class="input-xlarge" type="text" name="username" id="username" value="<?php echo set_value('username') ?>" required="required" />
 				</div>
 			</div>
 			
 			<?php endif; ?>
 			
-			<div class="control-group <?php echo iif( form_error('password') , 'error'); ?>">
+			<div class="control-group offset1 <?php echo iif( form_error('password') , 'error'); ?>">
 				<label class="control-label required" for="password"><?php echo lang('bf_password'); ?></label>
 				<div class="controls">
-					<input rel="tooltip" title="at least 8 characters" class="span4" type="password" name="password" id="password" value="" placeholder="<?php echo lang('bf_password'); ?>" required="required" />
+					<input rel="tooltip" title="8 or more characters" class="input-xlarge" type="password" name="password" id="password" value="" placeholder="8 or more characters" required="required" />
 				</div>
 			</div>
 			
-			<div class="control-group <?php echo iif( form_error('pass_confirm') , 'error'); ?>">
+			<div class="control-group offset1 <?php echo iif( form_error('pass_confirm') , 'error'); ?>">
 				<label class="control-label required" for="pass_confirm"><?php echo lang('bf_password_confirm'); ?></label>
 				<div class="controls">
-					<input class="span4" type="password" name="pass_confirm" id="pass_confirm" value="" placeholder="<?php echo lang('bf_password_confirm'); ?>" required="required" />
+					<input class="input-xlarge" type="password" name="pass_confirm" id="pass_confirm" value="" required="required" />
 				</div>
 			</div>
 			
 			
 			
-			<div class="control-group">
+			<div class="control-group offset1">
 				<label class="control-label required" for="birth_month"><?php echo lang('bf_birth_month'); ?></label>
 				<div class="controls">
-					<input id="birth_month" name="birth_month" class="span4" data-date-format="yyyy-mm" data-date-viewmode="2" data-date-minviewmode="1" value="<?php echo set_value('birth_month') ?>" />
+					<input id="birth_month" type="text" name="birth_month" class="input-xlarge" data-date-format="mm/yyyy" data-date-viewmode="1" data-date-minviewmode="2" value="<?php echo set_value('birth_month') ?>" />
 				</div>
 			</div>
-			<div class="control-group">
+			<div class="control-group offset1">
 				<label class="control-label required" for="zipcode"><?php echo lang('bf_zipcode'); ?></label>
 				<div class="controls">
-					<input value="<?php echo set_value('zipcode') ?>" class="span4" placeholder="<?php echo lang('bf_zipcode');?>" id="zipcode" name="zipcode"
+					<input type="text" value="<?php echo set_value('zipcode') ?>" class="input-xlarge" id="zipcode" name="zipcode"
 					value="" pattern="[0-9]{5}" maxlength="5" required="required"  /> 
 				</div>
 			</div>
-			<div class="control-group">
+			<div class="control-group offset1">
 				<label class="control-label required" for="race"><?php echo lang('bf_race'); ?></label>
 				<div class="controls"> 
-					<select id="race" class="span4" name="race" value="<?php echo set_value('race') ?>">
+					<select id="race" class="input-xlarge dropdown-register" name="race" value="<?php echo set_value('race') ?>">
 						<option value="">Please Select:</option>
 						<option value="White">White</option>
 						<option value="Black or African American">Black or African American</option>
@@ -90,10 +89,10 @@
 					</select>
 				</div>
 			</div>
-			<div class="control-group">
+			<div class="control-group offset1">
 				<label class="control-label required" for="education"><?php echo lang('bf_education'); ?></label>
 				<div class="controls">
-					<select id="education" class="span4" name="education">
+					<select id="education" class="input-xlarge" name="education">
 						<option value="">Please Select:</option>
 						<option value="PHD">PHD</option>
 						<option value="Master">Master</option>
@@ -103,29 +102,20 @@
 					</select> 
 				</div>
 			</div>
-			<div class="control-group">
+			
+			<div class="control-group offset1">
 				<label class="control-label required" for="first_name"><?php echo lang('bf_full_name'); ?></label>
 				<div class="controls"> 
-					<input value="<?php echo set_value('first_name') ?>"  type="text"  class="input-medium inline" placeholder="<?php echo lang('bf_first_name'); ?>" id="first_name"
+					<input value="<?php echo set_value('first_name') ?>"  type="text"  class="input-medium inline" placeholder="First name" id="first_name"
 					name="first_name" value="" pattern="[a-z A-Z]{1,25}" maxlength="25"
 					required="required" /> 
 					<input value="<?php echo set_value('last_name') ?>"
-					type="text" placeholder="<?php echo lang('bf_last_name'); ?>" id="last_name" name="last_name"
+					type="text" placeholder="Last name" id="last_name" name="last_name"
 					value="" pattern="[a-z A-Z]{1,25}" maxlength="25" class="input-medium inline" 
 					required="required" /> 
 				</div>
 			</div>
-			<div class="control-group">
-				<label class="control-label required" ><?php echo lang('bf_gender'); ?></label>
-				<div class="controls row-fluid">
-					<label class="radio inline" for="gender_f" >
-						<input
-					type="radio" id="gender_f" name="gender" value="0" /> <?php echo lang('bf_female'); ?></label>
-					<label class="radio inline" for="gender_m" >
-						<input
-					type="radio" id="gender_m" name="gender" value="1"/> <?php echo lang('bf_male'); ?></label>
-				</div>
-			</div>
+
 			<div class="row-fluid">
 				<div class="span8 offset2">
 					<div class="alert alert-info additional_info">
@@ -135,24 +125,25 @@
 					</div>
 				</div>
 			</div>
-			<?php echo form_dropdown('company_industry_id', $industry_dropdown,'',lang('bf_company_industry_id'),'required',$industry_dropdown_class);?>
-			
-			<div class="control-group">
-				<label class="control-label" for="occupation_id"><?php echo lang('bf_occupation'); ?></label>
-				<div class="controls">
-					<select id="occupation_id" class="span4" name="occupation_id">
-						<option value="">Please Select:</option>
-						<option value="1">Manager</option>
-						<option value="2">Student</option>
-						<option value="3">Teacher</option>
-						<option value="4">Other</option>
-					</select> 
+						
+			<div class="control-group offset1">
+				<label class="control-label required" ><?php echo lang('bf_gender'); ?></label>
+				<div class="controls row-fluid  additional_field">
+					<label class="radio inline" for="gender_f" >
+						<input
+					type="radio" id="gender_f" name="gender" value="0" /> <?php echo lang('bf_female'); ?></label>
+					<label class="radio inline" for="gender_m" >
+						<input
+					type="radio" id="gender_m" name="gender" value="1"/> <?php echo lang('bf_male'); ?></label>
 				</div>
 			</div>
+		
+			<?php echo form_dropdown('company_industry_id', $industry_dropdown,'',lang('bf_company_industry_id'),'offset1','required','class="input-xlarge additional_field"');?>
+
 			
-			<div class="control-group">
+			<div class="control-group offset1">
 				<label class="control-label" ><?php echo lang('bf_veteran'); ?></label>
-				<div class="controls row-fluid">
+				<div class="controls row-fluid additional_field">
 					<label class="radio inline" for="veteran_y" >
 						<input
 					type="radio" id="veteran_y" name="veteran" value="1" /> Yes</label>
@@ -162,7 +153,7 @@
 				</div>
 			</div>
 			
-			<div class="control-group">
+			<div class="control-group offset1">
 				<div class="controls">
 					<input class="btn btn-primary" type="submit" name="submit" id="submit" value="<?php echo lang('us_register'); ?>"  />
 				</div>
